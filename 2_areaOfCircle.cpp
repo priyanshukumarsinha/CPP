15c760afc34bae00b27aa46be3986d2c5c221a7e
priyanshukumarsinha <priyanshuk9066@gmail.com>
2022-09-25 11:23:46 +0530

    Program 2 : Area of Circle

---
+++ 2_areaOfCircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#define PI 3.14
+
+int main()
+{
+	int r;
+	std:: cout << "Enter radius of the cirle : ";
+	std:: cin >> r;
+	std:: cout << "Area of Circle is "<< PI*r*r << " units square"<< std:: endl;
+
+	return 0;
+}
