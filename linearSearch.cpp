c84bd143ffeb4b5a48a1aefb0c7f40d2b7bb69cc
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2025-06-04 10:04:57 +0000

    merge

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -156,7 +156,7 @@ void rightShift(struct Array *arr){
     }
 }
 
-void leftRotate(struct Array *arr){
+void rightRotate(struct Array *arr){
     int temp = arr->A[0];
     for(int i=1; i<arr->length; i++){
         arr->A[i-1] = arr->A[i];
@@ -164,14 +164,47 @@ void leftRotate(struct Array *arr){
     arr->A[arr->length-1] = temp;
 }
 
+struct Array * Merge(struct Array *arr1, struct Array *arr2){
+    int i,j,k;
+    i=j=k=0;
+
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i]<arr2->A[j]){
+            arr3->A[k] = arr1->A[i];
+            i++;
+        }
+        else{
+            arr3->A[k] = arr2->A[j];
+            j++;
+        }
+        k++;
+    }
+
+    for(; i<arr1->length; i++){
+        arr3->A[k] = arr1->A[i];
+        k++;
+    }
+
+    for(; j<arr2->length; j++){
+        arr3->A[k] = arr2->A[j];
+        k++;
+    }
+
+    arr3->length = arr2->length + arr1->length;
+    arr3->size = 10;
+
+    return arr3;
+}
+
 int main()
 {
- struct Array arr1={{2,23,14,5,6,9,8,12},10,8};
-//  cout << get(&arr1, 3) << endl;
-//  Set(&arr1,3,52);
-//  Display(arr1);
-//  cout << sum(&arr1) <<endl<< min(&arr1) <<endl<< max(&arr1)<<endl << avg(&arr1)<<endl;
- leftShift(&arr1);
- Display(arr1);
+ struct Array arr1={{2,9,21,28,35},10,5};
+ struct Array arr2={{2,3,16,18,28},10,5};
+ struct Array *arr3; 
+ arr3=Merge(&arr1,&arr2);
+ Display(*arr3);
+
  return 0;
 }
