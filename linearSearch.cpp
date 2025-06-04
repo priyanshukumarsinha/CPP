2ee4973b5e9bf9b2b88f368e867d6eaf65d4a516
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2025-06-04 08:34:17 +0000

    binarySearch

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -32,11 +32,30 @@ int LinearSearch(struct Array *arr, int key){
     return -1;
 }
 
+int binarySearch(struct Array *arr, int key){
+    int l, h, mid;
+    l=0;
+    h=arr->length -1;
+    while(l<=h){
+        mid = (l+h)/2;
+        if(arr->A[mid] == key){
+            return mid;
+        }
+        else if(arr->A[mid] < key){
+            h = mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+    }
+    return -1;
+}
+
 
 int main()
 {
  struct Array arr1={{2,23,14,5,6,9,8,12},10,8};
- printf("%d\n",LinearSearch(&arr1,14));
+ printf("%d\n",binarySearch(&arr1,14));
  Display(arr1);
  return 0;
 }
